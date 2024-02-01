b60f4c3c8d4c2eda2ece6214c7731f00446889bd
sirdav1d <ddavid.diniz@gmail.com>
2024-02-01 04:48:44 -0300

    teste

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -4,6 +4,6 @@ export const links = [
 	{ nameLink: 'Home', href: '/', slug: 'home' },
 	{ nameLink: 'Sobre Nós', href: '/about', slug: 'about' },
 	{ nameLink: 'Soluções', href: '/solutions', slug: 'solutions' },
-	{ nameLink: 'Projetos', href: '/projects', slug: 'projects' },
+	{ nameLink: 'Projetos', href: '/project', slug: 'project' },
 	{ nameLink: 'Contato', href: '/contact', slug: 'contact' },
 ];
