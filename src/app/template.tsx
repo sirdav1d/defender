11c156b1b367bf49780801e8bf6d7fe252c41108
sirdav1d <ddavid.diniz@gmail.com>
2024-02-22 01:46:00 -0300

    fix: :lipstick: remoçao de animate presence

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,25 +1,20 @@
 /** @format */
-'use client';
+
 import { MotionDiv } from '@/components/molecules/motionDIv';
 import React from 'react';
-import { AnimatePresence, easeInOut } from 'framer-motion';
 
 export default function Template({ children }: { children: React.ReactNode }) {
 	return (
-		<AnimatePresence mode='sync'>
-			{children && (
-				<MotionDiv
-					key={String(children)}
-					initial={{ opacity: 0.9, y: 28 }}
-					animate={{
-						opacity: 1,
-						y: 0,
-						transition: { delay: 0.2, duration: 0.8, ease: 'easeInOut' },
-					}}
-					exit={{ opacity: 0, y: 28 }}>
-					{children}
-				</MotionDiv>
-			)}
-		</AnimatePresence>
+		<MotionDiv
+			key={String(children)}
+			initial={{ opacity: 0.9, y: 28 }}
+			animate={{
+				opacity: 1,
+				y: 0,
+				transition: { delay: 0.2, duration: 0.8, ease: 'easeInOut' },
+			}}
+			exit={{ opacity: 0, y: 28 }}>
+			{children}
+		</MotionDiv>
 	);
 }
